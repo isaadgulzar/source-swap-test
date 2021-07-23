c03c8d2a9c37534627b9df5df9cd1b42b228fe5f
Saad Gulzar <47795736+isaadgulzar@users.noreply.github.com>
2021-07-23 23:55:13 +0500

    Add files via upload

---
+++ assets/js/sidebar.js
@@ -0,0 +1,8 @@
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+  
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+  
